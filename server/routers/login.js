052e2fd28d1a6be62aaf590f4e1dd8579cc58a66
Gustavo Tiseira <gustavotiseira@gmail.com>
2020-09-19 23:14:46 -0300

    Clase 121

--- server/routers/login.js
+++ server/routers/login.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 const Usuario = require('../models/usuario')
-const { post } = require('./usuario')
 const app = express()
 
 app.post('/login', function (req, res) {
-  res.json({
-    ok: true,
+  let body = req.body
+
+  Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err,
+      })
+    }
+
+    if (!usuarioDB) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: '(Usuario) o password incorrecta',
+        },
+      })
+    }
+
+    if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: 'Usuario o (password) incorrecta',
+        },
+      })
+    }
+
+    res.json({
+      ok: true,
+      usuario: usuarioDB,
+      token: 123,
+    })
   })
 })
 
